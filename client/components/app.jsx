96bbd10fc0ce690bf887101b48a7ed0d6f8472e3
Mil Amirian <milamirian22@gmail.com>
2020-09-21 22:02:00 +0000

    completed view all grades portion

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import PageTitle from './page-title';
+import GradesTable from './grades-table';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+    this.getAllGrades = this.getAllGrades.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllGrades();
+  }
+
+  getAllGrades() {
+    fetch('/api/grades')
+      .then(res => res.json())
+      .then(grade => {
+        this.setState({
+          grades: grade
+        });
+      });
+  }
+
   render() {
-    return null;
+    return (
+      <div className="container col-sm-12 col-lg-8">
+        <header className="col pt-3">
+          <PageTitle text="Student Grade Table"/>
+        </header>
+        <main>
+          <GradesTable grades={this.state.grades}/>
+        </main>
+      </div>
+    );
   }
 }
 
